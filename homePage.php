0566ff9b4298ea2c7e93d4345a7fcdc5dc1258f1
TuongNghiBui <camellia.bui95@gmail.com>
2023-08-16 16:49:24 +0200

    added code for login-logout

--- homePage.php
+++ homePage.php
@@ -1,3 +1,6 @@
+<?php include 'includes/login-logout.php';
+    require_login($logged_in);
+?>
 <?php include_once 'includes/navBar.php'?>
 
 <?php include_once 'includes/footer.php'?>
---
+++ includes/login-logout.php
@@ -0,0 +1,32 @@
+<?php 
+    session_start();
+    $logged_in = $_SESSION['logged_in'] ?? false;
+
+    $email = 'camellia.bui95@gmail.com';
+    $password = 'abc1234';
+
+    function login() {
+        // update session
+        session_regenerate_id(true);
+        $_SESSION['logged_in'] = true;
+    }
+
+    function logout() {
+        // clear server's session array;
+        $_SESSION = [];
+
+        // delete session cookie;
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+
+        // destroy session text file;
+        session_destroy();
+    }
+
+    function require_login($logged_in) {
+        if($logged_in == false) {
+            header('Location: login.php');
+            exit;
+        }
+    }
+?>
