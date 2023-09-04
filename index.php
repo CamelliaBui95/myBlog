f988bb72d9b21e371f7b510a7f1a4be724d8912e
TuongNghiBui <camellia.bui95@gmail.com>
2023-09-04 08:13:44 +0200

    progress 21%

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
     require_login($logged_in);
 ?>
+
 <?php include_once 'includes/header.php'?>
 <h1 class="mt-3">Home Page</h1>
 <?php include_once 'includes/footer.php'?>
---
+++ sqlScripts/loadMockData.sql
@@ -0,0 +1,9 @@
+USE `myblog`;
+
+--- Mock data for table categories --------------------
+INSERT INTO `categories` (`name`) VALUES("Technology");
+INSERT INTO `categories` (`name`) VALUES("Lifestyle");
+INSERT INTO `categories` (`name`) VALUES("Traveling");
+INSERT INTO `categories` (`name`) VALUES("Books");
+INSERT INTO `categories` (`name`) VALUES("Cinema");
+INSERT INTO `categories` (`name`) VALUES("Music");
